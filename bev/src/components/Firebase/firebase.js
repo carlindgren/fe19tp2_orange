3c298b53a0d30d2972d628a69ff04f2f04a65e02
Carl Lindgren <carlindgren@outlook.com>
2020-02-08 14:29:59 +0100

    firebase create account and login

---
+++ bev/src/components/Firebase/firebase.js
@@ -0,0 +1,37 @@
+import app from 'firebase/app';
+
+import 'firebase/auth'
+
+const config = {
+    apiKey: "AIzaSyCVKmDgAcXiqv1GXtU-ZHIeoGTNqbzuXbk",
+    authDomain: "fir-a1979.firebaseapp.com",
+    databaseURL: "https://fir-a1979.firebaseio.com",
+    projectId: "fir-a1979",
+    storageBucket: "fir-a1979.appspot.com",
+    messagingSenderId: "456958979479",
+    appId: "1:456958979479:web:6ed3835deae0ae465728d2",
+    measurementId: "G-0NFNB1W8R1"
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config)
+
+        this.auth = app.auth();
+    }
+    // **** Auth API ****
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password)
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email)
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password)
+}
+export default Firebase;
