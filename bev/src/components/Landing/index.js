847afc954e7c33ccc69852f78c6b2b285fc0d162
annalysethis <annalys01@gmail.com>
2020-02-17 14:14:03 +0100

    added styling to landingPage and navBa(Anna-branch)r

--- bev/src/components/Landing/index.js
+++ bev/src/components/Landing/index.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import SignUpButton from "./SignUpButton";
 
+const Landing = () => (
+  <LandingPage>
+    <Container>
+      <Title>Brottskollen</Title>
+      <InfoText>
+        Få exakta platsdata och färsk information om brott som sker i ditt
+        område.
+      </InfoText>
+      <SignUpButton />
+    </Container>
+  </LandingPage>
+);
 
-const Landing = () => {
-    return (
-        <div>
-            <h1>Landing</h1>
-        </div>
-    )
-}
+const LandingPage = styled.body`
+  background: url("https://images.unsplash.com/photo-1560575193-c2c9e886aefe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+  overflow: hidden;
+  opacity: 0.7;
+  z-index: -1;
+`;
+
+const Title = styled.h1`
+  color: white;
+  margin-top: 40%;
+  margin-bottom: 0;
+  font-family: "Montserrat Subrayada", sans-serif;
+`;
+
+const InfoText = styled.p`
+  margin-top: 0px;
+  font-size: 25px;
+  font-weight: bold;
+`;
+
+const Container = styled.div`
+  text-align: center;
+  color: white;
+  font-size: 30px;
+  text-shadow: 2px 2px black;
+  width: 60%;
+  margin: auto;
+`;
+
 export default Landing;
--- bev/src/components/Navigation/index.js
+++ bev/src/components/Navigation/index.js
@@ -1,64 +1,88 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import SignOutButton from '../SignOut';
-import * as ROUTES from '../../constants/routes';
-import { AuthUserContext } from '../Session';
-import * as ROLES from '../../constants/roles';
-
+import SignOutButton from "../SignOut";
+import * as ROUTES from "../../constants/routes";
+import { AuthUserContext } from "../Session";
+import * as ROLES from "../../constants/roles";
+import styled from "styled-components";
 
 const Navigation = () => (
-
-    <div>
-        <AuthUserContext.Consumer>
-            {authUser =>
-                authUser ? (
-                    <NavigationAuth authUser={authUser} />
-                ) : (
-                        <NavigationNonAuth />
-                    )
-            }
-        </AuthUserContext.Consumer>
-    </div>
+  <div>
+    <AuthUserContext.Consumer>
+      {authUser =>
+        authUser ? (
+          <NavigationAuth authUser={authUser} />
+        ) : (
+          <NavigationNonAuth />
+        )
+      }
+    </AuthUserContext.Consumer>
+  </div>
 );
 
 const NavigationAuth = ({ authUser }) => (
-
-    <ul>
-        <li>
-            <Link to={ROUTES.LANDING}>Landing</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.HOME}>Home</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.ACCOUNT}>Account</Link>
-        </li>
-        {authUser.roles.includes(ROLES.ADMIN) && (
-            <li>
-                <Link to={ROUTES.ADMIN}>Admin</Link>
-            </li>)}
-        <li>
-            <Link to={ROUTES.CHARTS}>Charts</Link>
-        </li>
-        <li>
-            <SignOutButton />
-        </li>
-    </ul>
+  <NavList>
+    <li>
+      <StyledLink to={ROUTES.LANDING}>Landing</StyledLink>
+    </li>
+    <li>
+      <StyledLink to={ROUTES.HOME}>Home</StyledLink>
+    </li>
+    <li>
+      <StyledLink to={ROUTES.ACCOUNT}>Account</StyledLink>
+    </li>
+    {authUser.roles.includes(ROLES.ADMIN) && (
+      <li>
+        <StyledLink to={ROUTES.ADMIN}>Admin</StyledLink>
+      </li>
+    )}
+    <li>
+      <StyledLink to={ROUTES.CHARTS}>Charts</StyledLink>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </NavList>
 );
-
 
 const NavigationNonAuth = () => (
-    <ul>
-        <li>
-            <Link to={ROUTES.LANDING}>Landing</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-        </li>
-    </ul>
+  <NavList>
+    <li>
+      <StyledLink to={ROUTES.LANDING}></StyledLink>
+    </li>
+    <li>
+      <StyledLink to={ROUTES.SIGN_IN}>Logga in</StyledLink>
+    </li>
+  </NavList>
 );
 
+const NavList = styled.ul`
 
+  li {
+    margin-top: 10px;
+  }
+  float: right;
+  margin: 0;
+  background: 
+  color: black;
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-end;
+  font-size: 15px;
+  list-style: none;
+  width: 50%;
+`;
+
+const StyledLink = styled(Link)`
+  list-style: none;
+  text-decoration: none;
+  color: black;
+  font-family: "Montserrat Subrayada", sans-serif;
+
+  &:hover {
+    color: #aaa;
+  }
+`;
 
 export default Navigation;
